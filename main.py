5b795e2de8e2fe37b99731257c32999e3ac1b1c4
LarryG0221 <76797293+LarryG0221@users.noreply.github.com>
2025-12-05 09:06:22 -0500

    display 'game over' when game fails
    display 'game over' when game fails

--- main.py
+++ main.py
@@ -13,25 +13,32 @@ import random
 def run_game():
     pygame.init()
     pygame.display.set_caption('Space Invader')
-    circulation = False if len(sys.argv) > 1 else True
 
     ai_setting = Settings()
     screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
 
+    # set bg image and smoothscale it
     background = pygame.image.load("images/bg.png")
     background = pygame.transform.smoothscale(background, (ai_setting.screen_width, ai_setting.screen_height))
 
+    # create a ship
     ship = Ship(screen, ai_setting)
 
+    # create group for aliens and bullets
     aliens = Group()
     alien_bullets = Group()
+
+    # save game statistics data
     stats = GameStats(ai_setting)
+
+    # place a button at the beginning
     play_button = Button(ai_setting, screen, 'Play')
+
+    # show score
     sb = Scoreboard(ai_setting, screen, stats)
 
     gf.create_fleet(ai_setting, screen, aliens, ship)
     screen.fill((0, 0, 0))
-
     while True:
         screen.blit(background, (0, 0))
 
@@ -39,9 +46,17 @@ def run_game():
         if stats.game_active:
             ship.update()
             gf.update_aliens(ai_setting, stats, screen, aliens, ship, sb, alien_bullets)
+        else:
+            if stats.ships_left == 0:
+                game_over_font = pygame.font.Font(None, 100)
+                game_over_text = game_over_font.render("GAME OVER", True, (255, 0, 0))
+
+                # center horizontally, near the top
+                x = ai_setting.screen_width // 2 - game_over_text.get_width() // 2
+                # middle of screen
+                y = 400
+                screen.blit(game_over_text, (x, y))
         gf.update_screen(screen, ship, aliens, alien_bullets, stats, play_button, sb )
-        if  not circulation:
-            break
 
         # save score in a text file
         with open('score.txt', 'w') as f:
@@ -49,5 +64,3 @@ def run_game():
         pygame.display.update()
 
 run_game()
-
-
